6ddc14f22276c2fee0d58c9196d755b73df0539f
DorukBatu <doruk.batu@outlook.com>
2018-11-21 16:15:58 +0000

    XUnitModified2

--- RCProject/Properties/AssemblyInfo.cs
+++ RCProject/Properties/AssemblyInfo.cs
@@ -22,6 +22,8 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a46ba21d-27bb-4ecb-ae6d-d1bf68938b03")]
 
+//adde
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
